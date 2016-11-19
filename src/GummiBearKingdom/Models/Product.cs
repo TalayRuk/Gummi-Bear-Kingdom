741c68d6ac6ef5802676b94a001e5ed3932737ef
vp <notejde@gmail.com>
2016-11-18 20:24:18 -0800

    add products & topics model

---
+++ src/GummiBearKingdom/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace GummiBearKingdom.Models
+{
+    [Table("Products")]
+    //Table Naming Convention need to be plural
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int Cost { get; set; }
+        public string Country_origin { get; set; }
+    }
+}
---
+++ src/GummiBearKingdom/Models/Topic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GummiBearKingdom.Models
+{
+    [Table("Topics")]
+    public class Topic
+    {
+        [Key]
+        public int TopicId { get; set; }
+        public string Subject { get; set; }
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
